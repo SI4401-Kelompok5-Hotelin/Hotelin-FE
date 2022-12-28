a4b6dbc95a5a9ff66328108c104acf272e86fafb
fatihabdi <dotaawtf23@gmail.com>
2022-12-28 23:11:44 +0700

    feat/fix navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-100 container">
+    <div className="navbar bg-base-100 container mb-8 mt-5">
       <div className="navbar-lg gap-3 ml-[40px]">
         <Link to="/">
           <h1 className="font-bold text-black">Home</h1>
@@ -20,17 +20,13 @@ export default function Header() {
         <img src={Logo} className="w-44"></img>
       </div>
       <div className="navbar-center -mr-[35px]">
-        <div className="card-actions mt-2">
-          <button className="btn bg-[#1788FB] border-none h-[43px]">
+          <button className="btn bg-[#1788FB] border-none h-[43px] w-[95px]">
             Log in
           </button>
-        </div>
 
-        <div className="card-actions mt-2 border-none">
           <button className="btn bg-white text-[#1788fb] border-none h-[43px]">
             Sign Up
           </button>
-        </div>
 
         <span className="badge badge-xs badge-primary indicator-item"></span>
       </div>
