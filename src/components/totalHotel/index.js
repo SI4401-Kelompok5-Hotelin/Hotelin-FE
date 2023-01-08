0887a1235dfab485b1ba4cf8ef309e30a3d3f41c
fatihabdi <dotaawtf23@gmail.com>
2023-01-08 19:57:29 +0700

    feat/add dashboard admin

---
+++ src/components/totalHotel/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TotalHotel() {
+  return (
+    <div className="lg:w-[321px] w-[200px] lg:mt-[13px] shadow-lg h-[196px] bg-white ml-[30px] rounded mt-5 flex flex-col items-center justify-center">
+      <h1 className="text-[31px] text-Primary-Normal font-bold">7</h1>
+      <p className="font-medium text-[20px] text-center text-Greyscale-Normal">
+        Total Hotel <br /> yang tersedia
+      </p>
+    </div>
+  );
+}
--- src/pages/DashboardAdmin/index.js
+++ src/pages/DashboardAdmin/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
+import TotalHotel from '../../components/totalHotel'
 
 export default function DashboardAdmin() {
   return (
     <div className='flex bg-white'>
       <Sidebar/>
       <div className='ml-[300px]'>
-        <h1>asdfghjkl</h1>
+        <TotalHotel />
       </div>
     </div>
   )
